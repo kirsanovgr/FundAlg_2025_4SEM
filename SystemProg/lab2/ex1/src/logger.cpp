9f6bc35e5edbf976db5b348abd19306b080a4929
Ecodrin <matvfo@ya.ru>
2025-04-15 07:38:31 +0300

    Formatting

--- SystemProg/lab2/ex1/src/logger.cpp
+++ SystemProg/lab2/ex1/src/logger.cpp
@@ -1,105 +1,77 @@
-#include <utility>
-
 #include "../include/logger.hpp"
 
+#include <utility>
 
 std::string Logger::get_datetime() {
-    auto now = std::chrono::system_clock::now();
-    auto time = std::chrono::system_clock::to_time_t(now);
-    std::tm timeBuf{};
+	auto now = std::chrono::system_clock::now();
+	auto time = std::chrono::system_clock::to_time_t(now);
+	std::tm timeBuf{};
 
-    localtime_r(&time, &timeBuf);
+	localtime_r(&time, &timeBuf);
 
-    std::ostringstream timeStream;
-    timeStream << std::put_time(&timeBuf, "%Y-%m-%d %H:%M:%S");
-    std::string timeStr = timeStream.str();
-    return timeStr;
+	std::ostringstream timeStream;
+	timeStream << std::put_time(&timeBuf, "%Y-%m-%d %H:%M:%S");
+	std::string timeStr = timeStream.str();
+	return timeStr;
 }
 
 void Logger::Log(Logger::LevelLogger level, const std::string &log) {
-    if (level > log_level) return;
-    std::string result = "[";
-    std::string time = get_datetime();
-    result += time + "][";
-    switch (level) {
-        case LOG_CRITICAL:
-            result += "CRITICAL]";
-            break;
-        case LOG_ERROR:
-            result += "ERROR]";
-            break;
-        case LOG_WARNING:
-            result += "WARNING]";
-            break;
-        case LOG_INFO:
-            result += "INFO]";
-            break;
-        case LOG_DEBUG:
-            result += "DEBUG]";
-            break;
-    }
-    result += " " + log;
-    for (auto &out: handlers) {
-        out->write(result);
-    }
+	if (level > log_level) return;
+	std::string result = "[";
+	std::string time = get_datetime();
+	result += time + "][";
+	switch (level) {
+		case LOG_CRITICAL:
+			result += "CRITICAL]";
+			break;
+		case LOG_ERROR:
+			result += "ERROR]";
+			break;
+		case LOG_WARNING:
+			result += "WARNING]";
+			break;
+		case LOG_INFO:
+			result += "INFO]";
+			break;
+		case LOG_DEBUG:
+			result += "DEBUG]";
+			break;
+	}
+	result += " " + log;
+	for (auto &out : handlers) {
+		out->write(result);
+	}
 }
 
-void Logger::LogError(const std::string &log) {
-    Log(LOG_ERROR, log);
-}
+void Logger::LogError(const std::string &log) { Log(LOG_ERROR, log); }
 
-void Logger::LogCritic(const std::string &log) {
-    Log(LOG_CRITICAL, log);
-}
+void Logger::LogCritic(const std::string &log) { Log(LOG_CRITICAL, log); }
 
-void Logger::LogWarning(const std::string &log) {
-    Log(LOG_WARNING, log);
-}
+void Logger::LogWarning(const std::string &log) { Log(LOG_WARNING, log); }
 
-void Logger::LogInfo(const std::string &log) {
-    Log(LOG_INFO, log);
-}
+void Logger::LogInfo(const std::string &log) { Log(LOG_INFO, log); }
 
-void Logger::LogDebug(const std::string &log) {
-    Log(LOG_DEBUG, log);
-}
+void Logger::LogDebug(const std::string &log) { Log(LOG_DEBUG, log); }
 
-void Logger::addHandler(std::unique_ptr<LogHandler> handler) {
-    handlers.push_back(std::move(handler));
-}
+void Logger::addHandler(std::unique_ptr<LogHandler> handler) { handlers.push_back(std::move(handler)); }
 
-void Logger::setHandler(Logger::LevelLogger level) {
-    log_level = level;
-}
+void Logger::setHandler(Logger::LevelLogger level) { log_level = level; }
 
-Logger::Logger(std::string logger_n, Logger::LevelLogger logger_level) : logger_name(std::move(logger_n)),
-                                                                                log_level(logger_level) {
+Logger::Logger(std::string logger_n, Logger::LevelLogger logger_level)
+    : logger_name(std::move(logger_n)), log_level(logger_level) {}
 
-}
-
-void Logger::close_logger() {
-    handlers.clear();
-
-}
+void Logger::close_logger() { handlers.clear(); }
 
 StreamLoggerHandler::StreamLoggerHandler(std::ostream &stream) : stream_{stream} {}
 
-void StreamLoggerHandler::write(const std::string &message) {
-    stream_ << message << std::endl;
-}
-
+void StreamLoggerHandler::write(const std::string &message) { stream_ << message << std::endl; }
 
 Logger *LoggerBuilder::build(const std::string &logger_name, Logger::LevelLogger logger_level) {
-    return new Logger{logger_name, logger_level};
+	return new Logger{logger_name, logger_level};
 }
 
-void FileLoggerHandler::write(const std::string &message) {
-    out << message << std::endl;
-}
+void FileLoggerHandler::write(const std::string &message) { out << message << std::endl; }
 
-FileLoggerHandler::FileLoggerHandler(const std::string &filename) : out(filename, std::ios::app) {
-}
+FileLoggerHandler::FileLoggerHandler(const std::string &filename) : out(filename, std::ios::app) {}
 
-FileLoggerHandler::~FileLoggerHandler() {
-    out.close();
-}
+FileLoggerHandler::~FileLoggerHandler() { out.close(); }
--- SystemProg/lab2/ex1/tests/test_logger.cpp
+++ SystemProg/lab2/ex1/tests/test_logger.cpp
@@ -1,11 +1,10 @@
 #include "../include/logger.hpp"
 
-
 int main() {
-    Logger *logger = LoggerBuilder::build("logger");
-    logger->addHandler(std::make_unique<FileLoggerHandler>("test.log"));
-    logger->addHandler(std::make_unique<StreamLoggerHandler>(std::cout));
-    for (int i = 0; i < 100; ++i) {
-        logger->Log(static_cast<Logger::LevelLogger>(rand() % 5), "asddsa");
-    }
+	Logger *logger = LoggerBuilder::build("logger");
+	logger->addHandler(std::make_unique<FileLoggerHandler>("test.log"));
+	logger->addHandler(std::make_unique<StreamLoggerHandler>(std::cout));
+	for (int i = 0; i < 100; ++i) {
+		logger->Log(static_cast<Logger::LevelLogger>(rand() % 5), "asddsa");
+	}
 }
