1894759b9c68428d28dda4f4a9692b124c366173
anxiousss <saimonkheresh@gmail.com>
2025-05-23 21:37:37 +0300

    micro fix container.hpp

--- Fund_alg/lab1/task1/include/container.hpp
+++ Fund_alg/lab1/task1/include/container.hpp
@@ -14,28 +14,7 @@ public:
     virtual Container &operator=(const Container &container) = 0;
 
     virtual ~Container() = default;
-    BigInt::BigInt(long long value, unsigned int base) : base(base) {
-        if (base % 10 != 0) {
-            throw std::invalid_argument("base must be power of 10\n");
-        }
-        isNegative = (value < 0);
 
-
-        unsigned long long abs_value = std::abs(value);
-        if (abs_value == 0) {
-            digits.push_back(0);
-        } else {
-            while (abs_value > 0) {
-                digits.push_back(abs_value % base);
-                abs_value /= base;
-            }
-        }
-
-        if (digits.empty()) {
-            digits.push_back(0);
-            isNegative = false;
-        }
-    }
     virtual bool operator==(const Container &other) const = 0;
     virtual bool operator!=(const Container &other) const = 0;
 
