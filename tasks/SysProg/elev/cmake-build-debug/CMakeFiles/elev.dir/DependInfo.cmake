a1ca28cf4e4eb7475ffddbd941c4e7723183e0ad
riloqq <llllesya22@gmail.com>
2025-06-05 07:49:08 +0300

    elevator 2.4

---
+++ tasks/SysProg/elev/cmake-build-debug/CMakeFiles/elev.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Олеся/CLionProjects/elev/main.cpp" "CMakeFiles/elev.dir/main.cpp.o" "gcc" "CMakeFiles/elev.dir/main.cpp.o.d"
+  "/mnt/c/Users/Олеся/CLionProjects/elev/src/el.cpp" "CMakeFiles/elev.dir/src/el.cpp.o" "gcc" "CMakeFiles/elev.dir/src/el.cpp.o.d"
+  "/mnt/c/Users/Олеся/CLionProjects/elev/src/logger.cpp" "CMakeFiles/elev.dir/src/logger.cpp.o" "gcc" "CMakeFiles/elev.dir/src/logger.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tasks/SysProg/elev/cmake-build-debug/CMakeFiles/elev.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elev.dir/main.cpp.o"
+  "CMakeFiles/elev.dir/main.cpp.o.d"
+  "CMakeFiles/elev.dir/src/el.cpp.o"
+  "CMakeFiles/elev.dir/src/el.cpp.o.d"
+  "CMakeFiles/elev.dir/src/logger.cpp.o"
+  "CMakeFiles/elev.dir/src/logger.cpp.o.d"
+  "elev"
+  "elev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
